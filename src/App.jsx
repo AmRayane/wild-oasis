4f5dd9f62decd008388a66f6d5515f6da785771d
Amligh Rayane <119620692+AmRayane@users.noreply.github.com>
2025-06-10 01:42:52 +0100

    supabase and react query

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,37 @@ import Settings from "./pages/Settings";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GloablStyles";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="dashboard" element={<Dashboard />} />
-            <Route path="booking" element={<Booking />} />
+            <Route path="bookings" element={<Booking />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
-            <Route path="Settings" element={<Settings />} />
+            <Route path="settings" element={<Settings />} />
           </Route>
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://telidragtsanvulczfcw.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlbGlkcmFndHNhbnZ1bGN6ZmN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0OTk5MjMsImV4cCI6MjA2NTA3NTkyM30.vXpvS__5h1bX4fWHcJ89YRmiudhr9jq9aVfsM-HfDns";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
